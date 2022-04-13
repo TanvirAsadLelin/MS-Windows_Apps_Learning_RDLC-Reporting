8250d14560a3b1f986053bd27b6ba841178c9e52
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-04-13 09:07:31 +0600

    RDLC Reporting Application added as an example

---
+++ RDLC_Reporting_WindowsFormsApp/RDLC_Reporting_WindowsFormsApp/ReportForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace RDLC_Reporting_WindowsFormsApp
+{
+    partial class ReportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.rDLC_Report_DBDataSet = new RDLC_Reporting_WindowsFormsApp.RDLC_Report_DBDataSet();
+            this.employeeInfoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.employeeInfoTableAdapter = new RDLC_Reporting_WindowsFormsApp.RDLC_Report_DBDataSetTableAdapters.EmployeeInfoTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.rDLC_Report_DBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeInfoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.employeeInfoBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "RDLC_Reporting_WindowsFormsApp.EmployeeInfoReport.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(840, 547);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // rDLC_Report_DBDataSet
+            // 
+            this.rDLC_Report_DBDataSet.DataSetName = "RDLC_Report_DBDataSet";
+            this.rDLC_Report_DBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // employeeInfoBindingSource
+            // 
+            this.employeeInfoBindingSource.DataMember = "EmployeeInfo";
+            this.employeeInfoBindingSource.DataSource = this.rDLC_Report_DBDataSet;
+            // 
+            // employeeInfoTableAdapter
+            // 
+            this.employeeInfoTableAdapter.ClearBeforeFill = true;
+            // 
+            // ReportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 547);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "ReportForm";
+            this.Text = "Employee Report Form";
+            this.Load += new System.EventHandler(this.ReportForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rDLC_Report_DBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeInfoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private RDLC_Report_DBDataSet rDLC_Report_DBDataSet;
+        private System.Windows.Forms.BindingSource employeeInfoBindingSource;
+        private RDLC_Report_DBDataSetTableAdapters.EmployeeInfoTableAdapter employeeInfoTableAdapter;
+    }
+}
+
---
+++ RDLC_Reporting_WindowsFormsApp/RDLC_Reporting_WindowsFormsApp/ReportForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RDLC_Reporting_WindowsFormsApp
+{
+    public partial class ReportForm : Form
+    {
+        public ReportForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ReportForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'rDLC_Report_DBDataSet.EmployeeInfo' table. You can move, or remove it, as needed.
+            this.employeeInfoTableAdapter.Fill(this.rDLC_Report_DBDataSet.EmployeeInfo);
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
